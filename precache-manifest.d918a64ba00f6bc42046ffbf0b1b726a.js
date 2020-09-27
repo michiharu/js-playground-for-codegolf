8508e65ca51f4050c43ae0d1aef6e1301b2462af
michiharu <michi1diver@gmail.com>
2020-09-28 01:55:34 +0900

    Updates

---
+++ precache-manifest.d918a64ba00f6bc42046ffbf0b1b726a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7d45f857ef26daa42fd5a66977ee47",
+    "url": "/js-playground-for-codegolf/index.html"
+  },
+  {
+    "revision": "06e508bce2efc8ffa5a4",
+    "url": "/js-playground-for-codegolf/static/js/2.8e9837c4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/js-playground-for-codegolf/static/js/2.8e9837c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ec1d5baa2512738538",
+    "url": "/js-playground-for-codegolf/static/js/main.b62a519b.chunk.js"
+  },
+  {
+    "revision": "b7fc578ca824206fa1d0",
+    "url": "/js-playground-for-codegolf/static/js/runtime-main.099ffe66.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/js-playground-for-codegolf/precache-manifest.d918a64ba00f6bc42046ffbf0b1b726a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/js-playground-for-codegolf/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
